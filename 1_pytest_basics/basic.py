3a377a92cd2c1700350d81dbd22865631a313488
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2022-05-24 21:12:09 +0100

    Set up project and wrote code for basics and data_integrity sections.

---
+++ 1_pytest_basics/basic.py
@@ -0,0 +1,17 @@
+def return_true():
+    """
+    Basic function that returns True
+    """
+    return True
+
+def add_10(num):
+    """
+    Adds 10 to the input number
+    """
+    return num + 10
+
+def throw_error():
+    """
+    Raises an value error 
+    """
+    raise ValueError
