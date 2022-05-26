b2f83a1a9c406511169210e93eb49214710b45de
joseph.wilson8-nhs <joseph.wilson8@nhs.net>
2022-05-26 15:01:04 +0100

    Whitespace fix

--- 2_advanced_pytest_features/2-2_parameterization/parameterization_test.py
+++ 2_advanced_pytest_features/2-2_parameterization/parameterization_test.py
@@ -3,7 +3,6 @@ To pass multiple arguments to the test function when parametrising a test, you c
 together into a list of tuples. If there is an uneven amount of items in a list (nums has 5, outputs
 has only 4) then the left over values will be ignored.
 '''
-
 import pytest
 
 nums = [1, 2, 3, 4, 5]
